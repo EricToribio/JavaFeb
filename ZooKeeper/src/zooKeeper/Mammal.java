39c69d0de90d0a99000c91f1f556b2eb6d40eb45
Eric <erictoribio2010@gmail.com>
2022-02-08 22:09:35 -0500

    zookeeper 1

---
+++ ZooKeeper/src/zooKeeper/Mammal.java
@@ -0,0 +1,25 @@
+package zooKeeper;
+
+public class Mammal {
+	 
+	private int energyLevel;
+	
+	public Mammal() {
+		this.energyLevel = 100;
+	}
+	
+	public int displayEnergyLevel() {
+		System.out.println(getEnergyLevel());
+		return getEnergyLevel();
+	}
+
+	public int getEnergyLevel() {
+		return energyLevel;
+	}
+
+	public void setEnergyLevel(int energyLevel) {
+		this.energyLevel = energyLevel;
+	}
+	
+
+}
